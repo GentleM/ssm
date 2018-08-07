8f5536688e222970232ebe22f1bd54795a97627f
rango <amazdreamer@163.com>
2018-08-07 14:56:09 +0800

    提交dao

---
+++ src/main/java/dao/business/ArticleDao.java
@@ -0,0 +1,18 @@
+package dao.business;
+
+import entity.Article;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 文章Dao。
+ *
+ * @author rango
+ *         2018-08-06 22:40
+ **/
+@Service
+public interface ArticleDao {
+
+    List<Article> getArticleList();
+}
